2c74d08168f698428b7e7122af8e722d8a445bb5
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-03-14 05:11:23 -0300

    stories ok

--- src/Stories.js
+++ src/Stories.js
@@ -1,12 +1,51 @@
 
+const dadosStories = [
+    {
+        foto: "./imgs/logo-animalplanet.jpg",
+        nome: "animalplanet"
+    },
+    {
+        foto: "./imgs/nathanwpylestrangeplanet 1.png",
+        nome: "strange"
+    },
+    {
+        foto: "./imgs/gatobranco.png",
+        nome: "cindy"
+    },
+    {
+        foto: "./imgs/logo-wiskas.jpg",
+        nome: "wiskas"
+    },
+    {
+        foto: "./imgs/gato-rave.jpeg",
+        nome: "yellow"
+    },
+    {
+        foto: "./imgs/catnip.jpg",
+        nome: "catnip"
+    },
+    {
+        foto: "./imgs/persa.jpg",
+        nome: "zeus"
+    },
+    {
+        foto: "./imgs/gatotiger.jpg",
+        nome: "tiger"
+    }
+]
+
 export default function Stories() {
     return (
-        <div className="back-ground-stories">
-            <img src="./imgs/stories_background 1.png" alt="back-ground-storie"></img>
-            <p>animalplanet</p>
-            <div className="stories-foto">
-                <img src="./imgs/logo-animalplanet.jpg" alt="storiesfoto"></img>
-            </div>
-        </div>
+        dadosStories.map(storie => {
+            return (
+                <div className="back-ground-stories">
+                    <img src="./imgs/stories_background 1.png" alt="back-ground-storie"></img>
+                    <p>{storie.nome}</p>
+                    <div className="stories-foto">
+                        <img src={storie.foto} alt="storiesfoto"></img>
+                    </div>
+                </div>
+            )
+        })
     )
 }
