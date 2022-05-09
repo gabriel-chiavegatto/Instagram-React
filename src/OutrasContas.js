bfa5a7e2bcf7faea355c2897c43fd1d90d758acb
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-05-09 13:31:29 -0300

    outras contas por map

--- src/OutrasContas.js
+++ src/OutrasContas.js
@@ -1,12 +1,36 @@
 
 export default function OutrasContas() {
+    const sugestoes = [
+        {
+            foto: "./imgs/gatoflocky-1.jpeg",
+            user: "baby.flocky",
+            status: "Novo no Instagram"
+        },
+        {
+            foto: "./imgs/smallcutecats 1.png",
+            user: "simba_cat",
+            status: "Segue você"
+        },
+        {
+            foto: "./imgs/chibirdart 1.png",
+            user: "naosougato",
+            status: "Segue você"
+        },
+        {
+            foto: "./imgs/budah.jpeg",
+            user: "budah_miau",
+            status: "Novo no Instagram"
+        },
+        {
+            foto: "./imgs/pedigree-logo-1.png",
+            user: "pedrigree",
+            status: "Segue você"
+        }
+    ];
+
     return (
         <div>
-            <CadaSugetão foto="./imgs/gatoflocky-1.jpeg" user="baby.flocky" status="Novo no Instagram" />
-            <CadaSugetão foto="./imgs/smallcutecats 1.png" user="simba_cat" status="Segue você" />
-            <CadaSugetão foto="./imgs/chibirdart 1.png" user="naosougato" status="Segue você" />
-            <CadaSugetão foto="./imgs/budah.jpeg" user="budah_miau" status="Novo no Instagram" />
-            <CadaSugetão foto="./imgs/pedigree-logo-1.png" user="pedrigree" status="Segue você" />
+            {sugestoes.map(conta => <CadaSugetão foto={conta.foto} user={conta.user} status={conta.status} key={conta.user}/>)}
         </div>
     )
 }
